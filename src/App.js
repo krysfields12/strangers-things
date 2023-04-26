1d9207a27aa6b7e5ddabcee834ce60d8ff2f0b95
Krystin Fields <krystinfields@Krystins-Laptop.local>
2023-04-26 14:03:52 -0400

    added to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { 
   Register,
   Posts,
   Login,
-  CreatePost
+  CreatePost,
+  NavBar
 } from './components'
 
-import { fetchPosts } from './api/posts';
+import { fetchPosts, myData } from './api/posts';
 
 
 export default function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
   const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState({})
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   
-    const getPosts = async () => {
+  const navigate = useNavigate();
+
+  const tokenCheck = () => {
+    if(window.localStorage.getItem("token")) {
+        setToken(window.localStorage.getItem("token"));
+    }
+  }
+
+  const getPosts = async () => {
       const result = await fetchPosts();
         if(result.success) {
             setPosts(result.data.posts)
         };
       };
+
+  const getMyData = async() => {
+    const results = await myData(token);
+    if(results.success) {
+        setUser(results.data);
+    }
+  }
 
       useEffect(() => {
         const tokenCheck = () => {
@@ -31,13 +49,22 @@ export default function App() {
       }, [])
 
       useEffect(() => {
-        getPosts()
+        getPosts();
+        if (token) {
+          getMyData();
+          setIsLoggedIn(true);
+        }
       }, [token]);
     
   
   
   return (
     <div>
+      <NavBar 
+        setToken={setToken}
+        setIsloggedIn={setToken}
+        isLoggedIn={isLoggedIn}
+      /> 
       <Routes>
         <Route 
           path='/' 
@@ -45,11 +72,11 @@ export default function App() {
         />
         <Route 
           path='/register' 
-          element={<Register setToken={setToken} />} 
+          element={<Register setToken={setToken} navigate={navigate} />} 
         />
         <Route
           path='/login'
-          element={<Login setToken={setToken} />}
+          element={<Login setToken={setToken} navigate={navigate}/>}
         />
         <Route 
           path='/create-post'
